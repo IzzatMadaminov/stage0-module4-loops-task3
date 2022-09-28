c0c1b6a0b05a509e0b231b3e4578447d113af8f3
Izzat Madaminov <izzat.madaminov@relentless.agency>
2022-09-28 10:27:07 +0500

    Task #3. For Loops

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
+    private long [] fibonacciNumbers;
     public void printFibonacci(int lastFibonacci) {
+        fibonacciNumbers = new long[lastFibonacci + 1];
+        getfibonaccis(lastFibonacci);
+        for (int i = 1; i <= lastFibonacci; i++) {
+            System.out.println(fibonacciNumbers[i]);
+        }
+    }
+
+    private long getfibonaccis(int n) {
+        if (n == 1) {
+            fibonacciNumbers[n] = 0;
+        } else  if (n == 2) {
+            fibonacciNumbers[n] = 1;
+        } else if (fibonacciNumbers[n] == 0) {
+            fibonacciNumbers[n] = getfibonaccis(n - 2) + getfibonaccis(n - 1);
+        }
+            return fibonacciNumbers[n];
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        System.out.println(gcd(first, second));
+    }
+
+    private int gcd(int first, int second) {
+        if (second == 0) {
+            return first;
+        } else {
+            return gcd(second, first % second);
+        }
     }
 }
